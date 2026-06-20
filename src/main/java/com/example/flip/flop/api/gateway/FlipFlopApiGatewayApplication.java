86bf50b7cb2f2b779b6e5d3485e8320e8c5dbbae
Suriyams3 <45282254+Suriyams3@users.noreply.github.com>
2026-06-20 21:37:39 +0530

    api-gateway initial commit

---
+++ src/main/java/com/example/flip/flop/api/gateway/FlipFlopApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.example.flip.flop.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipFlopApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlipFlopApiGatewayApplication.class, args);
+    }
+}
